fd9913c14d0ae18f194cddff33d75fc514bbcf13
Tiffany Conroy <tiffany.conroy@gmail.com>
2011-01-30 23:13:16 +0100

    Added height and width as global options

--- soundcloud-shortcode/soundcloud-shortcode.php
+++ soundcloud-shortcode/soundcloud-shortcode.php
@@ -49,41 +49,44 @@ function soundcloud_shortcode( $atts, $url='' ) {
        if ( empty( $url ) ) {
               extract(shortcode_atts( array(
                      'url' => '',
-                     'params' => '',
+                     'params' => soundcloud_build_params_string(),
                      'height' => '',
-                     'width' => '100%'
+                     'width'  => ''
               ), $atts ) );
        } else {
               extract(shortcode_atts(array(
-                     'params' => '',
+                     'params' => soundcloud_build_params_string(),
                      'height' => '',
-                     'width' => '100%'
+                     'width'  => ''
               ), $atts ) );
        }
-       
+
        $encoded_url = urlencode( $url );
-       
+
        if ( $url = parse_url( $url ) ){
               $splitted_url = split( "/", $url['path'] );
               $media_type = $splitted_url[count($splitted_url) - 2];
-       
-              if ( $height == "" ){
-                     if ( $media_type == "groups" || $media_type == "sets" ){
-                            $height = "225";
-                     } else {
-                            $height = "81";
-                     }
+
+              if ( $height == '' ) {
+                      if ( $media_type == "groups" || $media_type == "sets" ){
+                             $height = get_option('soundcloud_player_height_multi');
+                             if (!$height || $height == '') $height = '255';
+                      } else {
+                             $height = get_option('soundcloud_player_height', '81');
+                             if (!$height || $height == '') $height = '81';
+                      }
               }
-              
-              if ($params == '') {
-                    $params = soundcloud_build_params_string();
+
+              if ( $width == '' ) {
+                      $width = get_option('soundcloud_player_width');
+                      if (!$width || $width == '') $width =  '100%';
               }
-              
+
               $player_params = "url=$encoded_url&g=1&$params";
-              
+
               preg_match('/(.+\.)?(((staging|sandbox)-)?soundcloud\.com)/', $url['host'], $matches);
               $player_host = "player." . $matches[2];
-              
+
               return "<object height=\"" . esc_attr( $height ) . "\" width=\"" .
                      esc_attr( $width ) . "\"><param name=\"movie\" value=\"http://" .
                      esc_attr( $player_host ) . "/player.swf?" . esc_attr( $player_params ) .
@@ -105,14 +108,18 @@ function soundcloud_shortcode_options_menu() {
 }
 
 function register_soundcloud_settings() {
-       //register our settings
+       // register our settings
+       register_setting( 'soundcloud-settings', 'soundcloud_player_height' );
+       register_setting( 'soundcloud-settings', 'soundcloud_player_height_multi' );
+       register_setting( 'soundcloud-settings', 'soundcloud_player_width ' );
        register_setting( 'soundcloud-settings', 'soundcloud_auto_play' );
        register_setting( 'soundcloud-settings', 'soundcloud_show_comments ' );
        register_setting( 'soundcloud-settings', 'soundcloud_color' );
        register_setting( 'soundcloud-settings', 'soundcloud_theme_color' );
 }
 
-function soundcloud_build_params_string( $params='' ) {
+function soundcloud_build_params_string() {
+       $params  = '';
        $params .=  'auto_play='     . get_option( 'soundcloud_auto_play', '' );
        $params .= '&show_comments=' . get_option( 'soundcloud_show_comments ', '' );
        $params .= '&color='         . get_option( 'soundcloud_color', '' );
@@ -124,29 +131,54 @@ function soundcloud_shortcode_options() {
        if (!current_user_can('manage_options'))  {
               wp_die( __('You do not have sufficient permissions to access this page.') );
        }
-       
+
 // auto_play = (true or false)
 // show_comments = (true or false)
 // color = (color hex code) will paint the play button, waveform and selections in this color
 // theme_color = (color hex code) will set the background color
-       
+
 ?>
 <div class="wrap">
 <h2>SoundCloud Shortcode</h2>
-<p>These settings will become the new defaults used by the SoundCloud Shortcode throughout your blog.</p>    
+<p>These settings will become the new defaults used by the SoundCloud Shortcode throughout your blog.</p>
 <p>You can always override these settings on a per-shortcode basis. Setting the 'params' attribute in a shortcode overrides all these defaults combined.</p>
 
 <form method="post" action="options.php">
     <?php settings_fields( 'soundcloud-settings' ); ?>
     <table class="form-table">
-      
+
+        <tr valign="top">
+        <th scope="row">Player Height for Tracks</th>
+        <td>
+          <input type="text" name="soundcloud_player_height" value="<?php echo get_option('soundcloud_player_height'); ?>" /> (no unit, or %)<br />
+          Leave blank to use the default, 81 (pixels).
+        </td>
+        </tr>
+
+        <tr valign="top">
+        <th scope="row">Player Height for Groups/Sets</th>
+        <td>
+          <input type="text" name="soundcloud_player_height_multi" value="<?php echo get_option('soundcloud_player_height_multi'); ?>" /> (no unit, or %)<br />
+          Leave blank to use the default, 225 (pixels).
+        </td>
+        </tr>
+        
+        <tr valign="top">
+        <th scope="row">Player Width</th>
+        <td>
+          <input type="text" name="soundcloud_player_width" value="<?php echo get_option('soundcloud_player_width'); ?>" /> (no unit, or %)<br />
+          Leave blank to use the default, 100%.
+        </td>
+        </tr>
+
+
         <tr valign="top">
         <th scope="row">Current Default 'params'</th>
         <td>
             <?php echo soundcloud_build_params_string(); ?>
         </td>
         </tr>
-      
+
         <tr valign="top">
         <th scope="row">Auto Play</th>
         <td>
@@ -155,7 +187,7 @@ function soundcloud_shortcode_options() {
              <label for="auto_play_false" style="margin-right: 1em;"><input type="radio" id="auto_play_false" name="soundcloud_auto_play" value="false" <?php if (get_option('soundcloud_auto_play') == 'false') echo 'checked'; ?> />False</label>
         </td>
         </tr>
-         
+
         <tr valign="top">
         <th scope="row">Show Comments</th>
         <td>
@@ -163,7 +195,7 @@ function soundcloud_shortcode_options() {
              <label for="show_comments_true"  style="margin-right: 1em;"><input type="radio" id="show_comments_true"  name="soundcloud_show_comments" value="true"  <?php if (get_option('soundcloud_show_comments') == 'true')  echo 'checked'; ?> />True</label>
              <label for="show_comments_false" style="margin-right: 1em;"><input type="radio" id="show_comments_false" name="soundcloud_show_comments" value="false" <?php if (get_option('soundcloud_show_comments') == 'false') echo 'checked'; ?> />False</label>
         </tr>
-        
+
         <tr valign="top">
         <th scope="row">Color</th>
         <td>
@@ -171,17 +203,17 @@ function soundcloud_shortcode_options() {
           Defines the color to paint the play button, waveform and selections.
         </td>
         </tr>
-        
+
         <tr valign="top">
         <th scope="row">Theme Color</th>
         <td>
           <input type="text" name="soundcloud_theme_color" value="<?php echo get_option('soundcloud_theme_color'); ?>" /> (color hex code e.g. ff6699)<br />
-          Defines the background color of the player.          
+          Defines the background color of the player.
         </td>
         </tr>
 
     </table>
-    
+
     <p class="submit">
     <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
     </p>
@@ -189,7 +221,7 @@ function soundcloud_shortcode_options() {
 </form>
 </div>
 <?php
-       
+
 }
 
 ?>
